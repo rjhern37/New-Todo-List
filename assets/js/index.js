e8ffa00abf092fc662521c12433c8ada6dba03ff
Ray H <rjhern37@neiu.edu>
2020-08-03 11:47:16 -0500

    Finished JS, working on CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-alert('connected')
+//Made a click function to toggle a class on li's and future li's
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass('completed');
+});
+
+//Click function to delete/fadeout li's
+$('ul').on('click', 'span', function(event){
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+});
+
+//function to create new li with user input on keypress === enter
+$("#userInput").keypress(function(event){
+    if(event.which === 13){
+        //grabbing newTodo from input
+        var newTodo = $(this).val();
+        //input after newTodo has been entered
+        $(this).val('');
+        //create new li from newTodo
+        $('ul').append(`<li><span>X</span> ${newTodo}</li>`);
+    }
+});
